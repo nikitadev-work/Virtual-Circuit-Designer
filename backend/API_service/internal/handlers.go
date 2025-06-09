2e221fb4fab8f0b67d30fa0698fc6b2bdd4c2cee
NikitaKhripunkov <n.khripunkov@innopolis,university>
2025-06-09 17:20:15 +0300

    Created the structure of the API_service(handlers and main)

--- backend/API_service/internal/handlers.go
+++ backend/API_service/internal/handlers.go
@@ -1,1 +1,49 @@
 package internal
+
+import "net/http"
+
+func RegistrationHandler(w http.ResponseWriter, r *http.Request) {
+	_, write := w.Write([]byte("Registration"))
+	if write != nil {
+		return
+	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	_, write := w.Write([]byte("Login"))
+	if write != nil {
+		return
+	}
+}
+
+func CircuitsHandler(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == http.MethodGet:
+		GetAllCircuitsHandler(w, r)
+	case r.Method == http.MethodPost:
+		SaveNewCircuitHandler(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func GetAllCircuitsHandler(w http.ResponseWriter, r *http.Request) {
+	_, write := w.Write([]byte("Get All Circuits"))
+	if write != nil {
+		return
+	}
+}
+
+func SaveNewCircuitHandler(w http.ResponseWriter, r *http.Request) {
+	_, write := w.Write([]byte("Save New Circuit"))
+	if write != nil {
+		return
+	}
+}
+
+func StartSimulationHandler(w http.ResponseWriter, r *http.Request) {
+	_, write := w.Write([]byte("Start Simulation"))
+	if write != nil {
+		return
+	}
+}
--- backend/API_service/main.go
+++ backend/API_service/main.go
@@ -1,1 +1,18 @@
 package API_service
+
+import (
+	"API_service/internal"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/user/register", internal.RegistrationHandler)
+	http.HandleFunc("/user/login", internal.LoginHandler)
+	http.HandleFunc("/circuits", internal.CircuitsHandler)
+	http.HandleFunc("/circuits/simulate", internal.StartSimulationHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+}
