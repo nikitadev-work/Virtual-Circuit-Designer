636f2ebb78a36b6ed42f2f3858725ed25d0583bb
Gabdullin Amir <Amir.gabdullin2017@yandex.ru>
2025-06-10 18:18:45 +0300

    Added main.go and handlers.go to work with database

---
+++ backend/Database_service/internal/handlers/handlers.go
@@ -0,0 +1,13 @@
+package internal
+
+import (
+	"net/http"
+)
+
+db := storage.NewPostgresDB()
+
+func RegistrationHandler(resp http.ResponseWriter, req *http.Request) {
+	
+}
+func LoginHandler(resp http.ResponseWriter, req *http.Request)        {}
+func CircuitsHandler(resp http.ResponseWriter, req *http.Request)     {}
--- backend/Database_service/main.go
+++ backend/Database_service/main.go
@@ -1,1 +1,16 @@
-package Database_service
+package main
+
+import (
+	"Database_service/internal/handlers"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/db/user/register", handlers.RegistrationHandler)
+	http.HandleFunc("/db/user/login", handlers.LoginHandler)
+	http.HandleFunc("/db/circuits", handlers.CircuitsHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
