2e35f70bb630d9c1e84f1023c38b7c7b873f10af
NikitaKhripunkov <n.khripunkov@innopolis,university>
2025-06-11 13:41:43 +0300

    Finished Auth-service

--- backend/Authentication_service/internal/handlers.go
+++ backend/Authentication_service/internal/handlers.go
@@ -2,12 +2,18 @@ package internal
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 	"net/http"
 	"strings"
 )
 
 var secret_key = "our-secret-key"
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	jwt.RegisteredClaims
+}
 
 func CheckJWTTokenHandler(w http.ResponseWriter, r *http.Request) {
 	authHeader := r.Header.Get("Authorization")
@@ -22,6 +28,31 @@ func CheckJWTTokenHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	tokenString := parts[1]
+
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(secret_key), nil
+	})
+
+	if err != nil || !token.Valid {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return
+	}
+
+	if claims.UserID == "" {
+		http.Error(w, "Invalid token: missing user_id", http.StatusUnauthorized)
+		return
+	}
+
+	resp := Claims{UserID: claims.UserID}
+
+	json.NewEncoder(w).Encode(resp)
 }
 
 func GenerateJWTTokenHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,7 +75,7 @@ func GenerateJWTTokenHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Authorization", "Bearer "+tokenString)
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
 
 }
