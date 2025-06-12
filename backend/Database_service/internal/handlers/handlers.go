18a4539e59286cc50d528d3d91c90b047f46a1f8
Gabdullin Amir <Amir.gabdullin2017@yandex.ru>
2025-06-12 13:30:36 +0300

    feat: add circuits HTTP handlers
    - Implement POST /circuits for saving circuits
    - Implement GET /circuits for retrieving user circuits

--- backend/Database_service/internal/handlers/handlers.go
+++ backend/Database_service/internal/handlers/handlers.go
@@ -22,6 +22,26 @@ type RegistrationRequest struct {
 	Password string `json:"password"`
 }
 
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CircuitPostRequest struct {
+	User_id      int    `json:"user_id"`
+	Circuit_name string `json:"circuit_name"`
+	Circuit      string `json:"circuit"`
+}
+
+type CircuitGetRequest struct {
+	User_id int `json:"user_id"`
+}
+
+type Pair struct {
+	Circuit_name string
+	Circuit      string
+}
+
 func (h *DBHandler) RegistrationHandler(resp http.ResponseWriter, req *http.Request) {
 	var regReq RegistrationRequest
 	err := json.NewDecoder(req.Body).Decode(&regReq)
@@ -37,7 +57,51 @@ func (h *DBHandler) RegistrationHandler(resp http.ResponseWriter, req *http.Requ
 	}
 
 	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusCreated)
 	json.NewEncoder(resp).Encode(map[string]int{"id": id})
 }
-func (h *DBHandler) LoginHandler(resp http.ResponseWriter, req *http.Request)    {}
-func (h *DBHandler) CircuitsHandler(resp http.ResponseWriter, req *http.Request) {}
+func (h *DBHandler) LoginHandler(resp http.ResponseWriter, req *http.Request) {
+	var logReq LoginRequest
+	err := json.NewDecoder(req.Body).Decode(&logReq)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id, err := h.db.GetUser(logReq.Email, logReq.Password)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	resp.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(resp).Encode(map[string]int{"id": id})
+}
+func (h *DBHandler) CircuitsHandler(resp http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		var circuitPostReq CircuitPostRequest
+		err := json.NewDecoder(req.Body).Decode(&circuitPostReq)
+		if err != nil {
+			http.Error(resp, err.Error(), http.StatusBadRequest)
+			return
+		}
+		err = h.db.SaveCircuits(circuitPostReq.User_id, circuitPostReq.Circuit_name, circuitPostReq.Circuit)
+		if err != nil {
+			http.Error(resp, err.Error(), http.StatusBadRequest)
+			return
+		}
+		resp.WriteHeader(http.StatusCreated)
+	case http.MethodGet:
+		var circuitGetReq CircuitGetRequest
+		err := json.NewDecoder(req.Body).Decode(&circuitGetReq)
+		if err != nil {
+			http.Error(resp, err.Error(), http.StatusBadRequest)
+		}
+
+		circuits, err := h.db.GetCircuit(circuitGetReq.User_id)
+
+		resp.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(resp).Encode(circuits)
+	}
+}
--- backend/Database_service/internal/storage/storage.go
+++ backend/Database_service/internal/storage/storage.go
@@ -15,11 +15,24 @@ type User struct {
 	password string
 }
 
+type Circuit struct {
+	id           int
+	user_id      int
+	circuit_name string
+	circuit      string
+}
+
 type PostgresDB struct {
 	conn *sql.DB
 }
 
-var id int
+type Pair struct {
+	Circuit_name string `json:"circuit_name"`
+	Circuit      string `json:"circuit"`
+}
+
+var userId int
+var circuitID int
 
 func NewPostgresDB() *PostgresDB {
 	connStr := "" // user, password, dbname etc.
@@ -32,25 +45,53 @@ func NewPostgresDB() *PostgresDB {
 }
 
 func (db *PostgresDB) CreateUser(username, email, password string) (int, error) {
-	id += 1
-	_, err := db.conn.Exec("insert into Users (id, name, email, password) values ($1, $2, $3, $4)", id, username, email, password)
-	return id, err
+	userId += 1
+	_, err := db.conn.Exec("insert into Users (id, name, email, password) values ($1, $2, $3, $4)", userId, username, email, password)
+	return userId, err
 }
 
-func (db *PostgresDB) GetUserByEmail(email string) (User, error) {
-	query := "Select id, name, email, password, circuits FROM Users WHERE email = $1"
-	row := db.conn.QueryRow(query, email)
+func (db *PostgresDB) GetUser(email, password string) (int, error) {
+	query := "Select id, name, email, password, circuits FROM Users WHERE email = $1 AND password = $2"
+	row := db.conn.QueryRow(query, email, password)
 
-	var NotFoundUser User
 	var foundUser User
 	err := row.Scan(&foundUser.id, &foundUser.name, &foundUser.email, &foundUser.password)
 
 	switch {
 	case err == sql.ErrNoRows:
-		return NotFoundUser, fmt.Errorf("User with email '%s' not found", email)
+		return 0, fmt.Errorf("User with email '%s' not found", email)
 	case err != nil:
-		return NotFoundUser, fmt.Errorf("Database error: '%v'", err)
+		return 0, fmt.Errorf("Database error: '%v'", err)
 	default:
-		return foundUser, err
+		return foundUser.id, err
 	}
+}
+
+func (db *PostgresDB) SaveCircuits(user_id int, circuit_name, circuit string) error {
+	circuitID += 1
+	_, err := db.conn.Exec("Insert into Circuits (id, user_id, name_of_scheme, scheme) values ($1, $2, $3, $4)", circuitID, user_id, circuit_name, circuit)
+	return err
+}
+
+func (db *PostgresDB) GetCircuit(user_id int) ([]Pair, error) {
+	query := "Select * FROM Circuits WHERE user_id = $1"
+	rows, err := db.conn.Query(query, user_id)
+	if err != nil {
+		return []Pair{}, err
+	}
+
+	var circuitsFound []Pair
+	for rows.Next() {
+		var c Circuit
+		var temp Pair
+		err := rows.Scan(&c.id, &c.user_id, &c.circuit_name, &c.circuit)
+		if err != nil {
+			return []Pair{}, err
+		}
+		temp.Circuit_name = c.circuit_name
+		temp.Circuit = c.circuit
+		circuitsFound = append(circuitsFound, temp)
+	}
+
+	return circuitsFound, nil
 }
