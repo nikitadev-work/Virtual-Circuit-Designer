2616492cbe0e466637180f1eb508f3ecf4c3c1b3
Islam <is.gainullin@innopolis.university>
2025-06-13 12:57:49 +0300

    bam

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,18 @@
-import { GalleryVerticalEnd } from "lucide-react"
+import { Button } from "bim/components/ui/button"
+import Link from "next/link";
+import '../styles/global.css';
 
-import { LoginForm } from "bim/components/login-form"
-
-export default function LoginPage() {
+export default function MainPage() {
     return (
-        <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div className="flex w-full max-w-sm flex-col gap-6">
-                <a href="#" className="flex items-center gap-2 self-center font-medium">
-                    <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-                        <GalleryVerticalEnd className="size-4" />
-                    </div>
-                    Acme Inc.
-                </a>
-                <LoginForm />
+        <>
+            <div className="">
+                Visual Circuit Designer
             </div>
-            <div className="bg-red-500 text-white p-4">Hello Tailwind!</div>
-        </div>
+            <div className="flex flex-wrap items-center gap-2 md:flex-row">
+                <Link href="/login" passHref>
+                    <Button>Login</Button>
+                </Link>
+            </div>
+        </>
     )
 }
