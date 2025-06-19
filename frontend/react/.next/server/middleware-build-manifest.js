3f75933c8b3aa65f13552ac0ccbbaf396dfd4ad2
Islam <is.gainullin@innopolis.university>
2025-06-19 22:01:53 +0300

    Registration + Login functionality working

--- frontend/react/.next/server/middleware-build-manifest.js
+++ frontend/react/.next/server/middleware-build-manifest.js
@@ -2,34 +2,13 @@ globalThis.__BUILD_MANIFEST = {
   "polyfillFiles": [
     "static/chunks/polyfills.js"
   ],
-  "devFiles": [
-    "static/chunks/react-refresh.js"
-  ],
+  "devFiles": [],
   "ampDevFiles": [],
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "rootMainFilesTree": {},
   "pages": {
-    "/_app": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_app.js"
-    ],
-    "/_error": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_error.js"
-    ],
-    "/login": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/login.js"
-    ],
-    "/playground": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/playground.js"
-    ]
+    "/_app": []
   },
   "ampFirstPages": []
 };
