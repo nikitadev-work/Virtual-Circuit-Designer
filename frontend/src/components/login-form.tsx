fb699b4b0d4bd22aa606ab05e787135f2a42fc89
NikitaKhripunkov <n.khripunkov@innopolis,university>
2025-07-01 20:54:47 +0300

    fix

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -38,12 +38,13 @@ export function LoginForm({ className, ...props }: React.ComponentProps<"div">) 
     const HOST = window.location.host;
 
     try {
-      const response = await fetch('http://' + HOST + '/api/user/login', {
+      const response = await fetch('http://' + HOST + ':8052/api/user/login', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
         body: JSON.stringify({email, password}),
       });
 
