ed34f435f0c4823dae5489ddf870a65ca5f9d409
Alex Today <ernestkudakaev6@mail.ru>
2025-07-18 12:05:48 +0300

    CorrectOfWorkOfCTXmenu

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
                 <a href="#" className="flex items-center gap-2 self-center font-medium">
-                    <img src="/Icons/Logos/Logo.svg" width={20} height={20} alt="Logo"/>
+                    <Image src="/Icons/Logos/Logo.svg" width={20} height={20} alt="Logo"/>
                     Visual Circuit Designer
                 </a>
                 <LoginForm />
